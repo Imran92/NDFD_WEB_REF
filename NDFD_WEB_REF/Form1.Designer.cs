5969c1454714112cc11f3a06f3b2a4a397d2fafa
imranhossain <imranh920@gmail.com>
2017-06-27 14:18:19 +0600

    Added all files, working

---
+++ NDFD_WEB_REF/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace NDFD_WEB_REF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(703, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(13, 30);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(684, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(13, 78);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(892, 501);
+            this.outputBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 591);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+    }
+}
+
---
+++ NDFD_WEB_REF/Form1.cs
@@ -0,0 +1,42 @@
+﻿using GoogleMaps.LocationServices;
+using NDFD_WEB_REF.gov.weather.graphical;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NDFD_WEB_REF
+{
+    public partial class Form1 : Form
+    {
+        double lat;
+        double lon;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getLongitudeLatitudeFromAddress();
+            ndfdXML ndfd = new ndfdXML();
+            ndfd.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36";
+            outputBox.Text = ndfd.NDFDgen(Convert.ToDecimal(lat), Convert.ToDecimal(lon), "time-series", DateTime.Now.Date, DateTime.Now.Date, string.Empty, null);
+        }
+        private void getLongitudeLatitudeFromAddress()
+        {
+            var locationService = new GoogleLocationService();
+            var address = inputBox.Text;
+            var point = locationService.GetLatLongFromAddress(address);
+
+            lat = point.Latitude;
+            lon = point.Longitude;
+        }
+    }
+}
